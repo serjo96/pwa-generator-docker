cdd27130862555c4154581abdfcd88072a4ce60b
serjo96 <king-beer@yandex.ru>
2019-10-30 21:59:39 +0500

    Translate to eng

--- front-end/src/Helpers/FormValidate.ts
+++ front-end/src/Helpers/FormValidate.ts
@@ -1,8 +1,8 @@
-export const requireField = (value: string ) => !!value || 'Обязательное поле.';
-export const emailRequired = (value: string ) => !!value || 'Необходимо ввести E-mail.';
+export const requireField = (value: string ) => !!value || 'Require Field.';
+export const emailRequired = (value: string ) => !!value || 'Require E-mail.';
 export const hexColor = (value: string ) => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/igm.test(value) || 'Color must be in hex';
 export const urlValidate = (value: string ) => /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/.test(value) || 'Url must be valid';
-export const validEmailRequired =  (value: string ) => /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(value) || 'E-mail должен быть валидным';
+export const validEmailRequired =  (value: string ) => /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(value) || 'E-mail must be valid';
 export const validateImageSize = (fsRes: any, size: { fixSize?: number, min?: number, max?: number }) => {
 	return new Promise((resolve, reject) => {
 		const img = new Image();
